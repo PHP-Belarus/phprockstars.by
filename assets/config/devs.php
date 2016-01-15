653d80ddc72461475fe660113d96b2045f4f215d
Igor Cheledinov <cheledinov@gmail.com>
2016-01-15 22:14:02 +0300

    Adds php by starts

--- assets/config/devs.php
+++ assets/config/devs.php
@@ -1,20 +1,30 @@
 <?php
 
-return array (
+return array(
 
-  array (
-    'name' => 'Dmitrii Varvashenya',
-    'desc' => 'Drupal modules contributor',
-    'twitter' => 'varvashenia',
-  ),
-  array(
-    'name' => 'Maslovskiy Evgeniy',
-    'desc' => 'Drupal modules contributor',
-    'twitter' => 'Spleshka'
-  ),
-  array(
-    'name' => 'Aleh Kot',
-    'desc' => 'Expert in PHP stack',
-    'twitter' => 'k0teg'
-  )
+    array(
+        'name' => 'Dmitrii Varvashenya',
+        'desc' => 'Drupal modules contributor',
+        'twitter' => 'varvashenia',
+    ),
+    array(
+        'name' => 'Maslovskiy Evgeniy',
+        'desc' => 'Drupal modules contributor',
+        'twitter' => 'Spleshka',
+    ),
+    array(
+        'name' => 'Aleh Kot',
+        'desc' => 'Expert in PHP stack',
+        'twitter' => 'k0teg',
+    ),
+    array(
+        'name' => 'Ivan Klimchuk',
+        'desc' => 'MODX Ambassador in Belarus',
+        'twitter' => 'iklimchuk',
+    ),
+    array(
+        'name' => 'Anton Serdyuk',
+        'desc' => 'Contributor to Laravel, Doctrine and PHP-Fig',
+        'twitter' => 'anton_serdyuk',
+    ),
 );
